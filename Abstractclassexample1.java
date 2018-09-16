39db3fc54fa1d8e13911bf3cbff1512011e45b2b
PRANEETHSWIFTGOULDING <33199516+PRANEETHSWIFTGOULDING@users.noreply.github.com>
2018-09-16 13:45:51 +0530

    Add files via upload

---
+++ Abstractclassexample1.java
@@ -0,0 +1,48 @@
+package abstractclassexample1;
+
+abstract class Gameconsole{
+int i=10;
+ void display(){
+System.out.println("The score is");
+}
+ abstract void playgame();
+ public abstract void playgame2();
+
+}
+class A extends Gameconsole {
+    void start(){
+System.out.println("GAME Started");
+    }
+   void  playgame(){
+   System.out.println("Lets play the Game");
+   }
+
+   
+    public void playgame2() {
+      System.out.println("Lets play the Game 2");
+    }
+   
+}
+class B extends A {
+
+    @Override
+    void playgame() {
+      System.out.println("Lets play the Game  in A");
+
+    }
+
+    @Override
+    public void playgame2() {
+         System.out.println("Lets play the Game 2 in B");
+         display();
+         start();
+         
+ }
+}
+public class Abstractclassexample1 {
+    public static void main(String[] args) {
+    B b=new B();
+    b.playgame2();
+    }
+    
+}
